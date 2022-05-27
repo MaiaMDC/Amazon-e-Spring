83057c57c29cc60537e1c9086b3752755fd08736
Marcio Souza <marciodouglas.camargo@gmail.com>
2022-05-27 20:50:11 -0300

    Insercao da classe de integracao Amazon

---
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/configuration/AmazonConfiguration.java
@@ -0,0 +1,8 @@
+package br.com.casadocodigo.loja.configuration;
+
+public class AmazonConfiguration {
+
+	private static final String ACCESS_KEY="ASFG";
+	private static final String SECRET_KEY="asdfg";
+	
+}
