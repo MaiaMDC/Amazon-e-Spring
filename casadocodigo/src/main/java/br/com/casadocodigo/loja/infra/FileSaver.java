eb21f797f8cad6d869dd32d0ad16f2d27e06530a
Marcio Souza <marciodouglas.camargo@gmail.com>
2022-06-01 23:29:47 -0300

    iniciando a migracao do servidor para o aws

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/infra/FileSaver.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/infra/FileSaver.java
@@ -1,29 +1,22 @@
 package br.com.casadocodigo.loja.infra;
 
-import java.io.File;
 import java.io.IOException;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-import br.com.casadocodigo.loja.models.Produto;
+import com.amazonaws.services.s3.AmazonS3;
 
 @Component
 public class FileSaver {
 
 	@Autowired
-	private HttpServletRequest request;
+	private AmazonS3 amazonS3;
 
 	public String write(MultipartFile file) {
 		try {
-			File arquivo = new File(request.getServletContext().getRealPath(
-					"/resources/imagens"), file.getOriginalFilename());
-			file.transferTo(arquivo);
 			
-			return file.getOriginalFilename();
 			
 		} catch (IllegalStateException | IOException e) {
 			throw new RuntimeException(e);
